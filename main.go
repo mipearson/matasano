f092a770248128ff117a88d947033abaabbffb08
mipearson <mipearson@gmail.com>
2015-01-25 21:38:03 +1100

    Pass test for challenge #1

--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
-func HexToBase64(hex []byte) []byte {
-	// var num uint32
-	return []byte("")
+import "log"
+import "encoding/hex"
+import "encoding/base64"
+
+func checkerr(err error) {
+	if err != nil {
+		log.Fatalf("Error encountered: %q", err)
+	}
+}
+func HexToBase64(src []byte) []byte {
+	raw := make([]byte, hex.DecodedLen(len(src)))
+
+	_, err := hex.Decode(raw, src)
+	checkerr(err)
+
+	enc := base64.StdEncoding
+	dst := make([]byte, enc.EncodedLen(len(raw)))
+	enc.Encode(dst, raw)
+	return dst
 }
