da67da12bd1650258badd3dd6e2829fc5ec8fea3
Michael Pearson <mipearson@gmail.com>
2015-01-27 11:58:51 +1100

    Set 1, challenge 2

--- main.go
+++ main.go
@@ -9,14 +9,37 @@ func checkerr(err error) {
 		log.Fatalf("Error encountered: %q", err)
 	}
 }
-func HexToBase64(src []byte) []byte {
-	raw := make([]byte, hex.DecodedLen(len(src)))
 
-	_, err := hex.Decode(raw, src)
+func hexDecode(src []byte) []byte {
+	dst := make([]byte, hex.DecodedLen(len(src)))
+	_, err := hex.Decode(dst, src)
 	checkerr(err)
 
+	return dst
+}
+
+func hexEncode(src []byte) []byte {
+	dst := make([]byte, hex.EncodedLen(len(src)))
+	_ = hex.Encode(dst, src)
+	return dst
+}
+
+func HexToBase64(src []byte) []byte {
+
+	raw := hexDecode(src)
 	enc := base64.StdEncoding
 	dst := make([]byte, enc.EncodedLen(len(raw)))
 	enc.Encode(dst, raw)
 	return dst
+}
+
+func XorHex(a []byte, b []byte) []byte {
+	ab := hexDecode(a)
+	bb := hexDecode(b)
+
+	c := make([]byte, len(ab))
+	for i := 0; i < len(c); i++ {
+		c[i] = ab[i] ^ bb[i]
+	}
+	return hexEncode(c)
 }
--- main_test.go
+++ main_test.go
@@ -1,14 +1,25 @@
 package main
 
 import "testing"
-import "reflect"
-
-const Hex string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-const Base64 string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
+import "bytes"
 
 func TestHexToBase64(t *testing.T) {
-	got := HexToBase64([]byte(Hex))
-	if !reflect.DeepEqual(got, []byte(Base64)) {
-		t.Errorf("HexToBase64(%q) == %q, want %q", Hex, string(got), Base64)
+	hex := []byte("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d")
+	base64 := []byte("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+
+	got := HexToBase64(hex)
+	if !bytes.Equal(got, base64) {
+		t.Errorf("HexToBase64(%s) == %s, want %s", hex, got, base64)
+	}
+}
+
+func TestXorHex(t *testing.T) {
+	orig := []byte("1c0111001f010100061a024b53535009181c")
+	xor := []byte("686974207468652062756c6c277320657965")
+	expected := []byte("746865206b696420646f6e277420706c6179")
+
+	got := XorHex(orig, xor)
+	if !bytes.Equal(got, expected) {
+		t.Errorf("XorHex(%s, %s) == %s, want %s", orig, xor, got, expected)
 	}
 }
