9bb93cbda063b46bc78c2efebcb36e56486b75d9
Michael Pearson <mipearson@gmail.com>
2015-01-30 14:52:09 +1100

    Fix slice sizes on decoded hex/base64 items

--- data.go
+++ data.go
@@ -3,17 +3,20 @@ package matasano
 import (
 	"encoding/base64"
 	"encoding/hex"
+	"regexp"
 )
 
 type Base64 []byte
 type Hex []byte
 
+var WhitespaceRegexp = regexp.MustCompile("\\s+")
+
 func (h Hex) Decode() []byte {
 	dst := make([]byte, hex.DecodedLen(len(h)))
-	_, err := hex.Decode(dst, h)
+	len, err := hex.Decode(dst, h)
 	checkerr(err)
 
-	return dst
+	return dst[:len]
 }
 
 func ToHex(src []byte) Hex {
@@ -23,15 +26,14 @@ func ToHex(src []byte) Hex {
 }
 
 func ToBase64(src []byte) Base64 {
-	enc := base64.StdEncoding
-	dst := make(Base64, enc.EncodedLen(len(src)))
-	enc.Encode(dst, src)
+	dst := make(Base64, base64.StdEncoding.EncodedLen(len(src)))
+	base64.StdEncoding.Encode(dst, src)
 	return dst
 }
 
 func (b Base64) Decode() []byte {
-	enc := base64.StdEncoding
-	dst := make([]byte, enc.DecodedLen(len(b)))
-	enc.Decode(dst, b)
-	return dst
+	dst := make([]byte, base64.StdEncoding.DecodedLen(len(b)))
+	len, err := base64.StdEncoding.Decode(dst, b)
+	checkerr(err)
+	return dst[:len]
 }
