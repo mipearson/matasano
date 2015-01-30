c907e8d9e993a54a231486a04388e1d86fe08364
Michael Pearson <mipearson@gmail.com>
2015-01-30 22:00:34 +1100

    Set 2, Challenge 10

--- main.go
+++ main.go
@@ -196,6 +196,53 @@ func DecryptAESECB(cipher []byte, key []byte) []byte {
 	return dst
 }
 
+func EncryptAESECB(plaintext []byte, key []byte) []byte {
+	aes, err := aes.NewCipher(key)
+	checkerr(err)
+
+	dst := make([]byte, len(plaintext))
+	for i := 0; i < len(dst); i += aes.BlockSize() {
+		aes.Encrypt(dst[i:], plaintext[i:])
+	}
+	return dst
+}
+
+func DecryptAESCBC(cipher []byte, key []byte, iv []byte) []byte {
+	aes, err := aes.NewCipher(key)
+	checkerr(err)
+
+	if len(iv) != aes.BlockSize() {
+		panic("aes.BlockSize != len(iv)")
+	}
+
+	dst := make([]byte, len(cipher))
+	for i := 0; i < len(dst); i += aes.BlockSize() {
+		til := i + aes.BlockSize()
+		aes.Decrypt(dst[i:], cipher[i:])
+		copy(dst[i:til], Xor(dst[i:til], iv))
+		iv = cipher[i:]
+	}
+	return dst
+}
+
+func EncryptAESCBC(plaintext []byte, key []byte, iv []byte) []byte {
+	aes, err := aes.NewCipher(key)
+	checkerr(err)
+
+	if len(iv) != aes.BlockSize() {
+		panic("aes.BlockSize != len(iv)")
+	}
+
+	dst := make([]byte, len(plaintext))
+	for i := 0; i < len(dst); i += aes.BlockSize() {
+		til := i + aes.BlockSize()
+		part := Xor(iv, plaintext[i:til])
+		aes.Encrypt(dst[i:], part)
+		iv = dst[i:til]
+	}
+	return dst
+}
+
 func DiscoverECB(cipher []byte, keysize int) bool {
 	for a := 0; a < len(cipher); a += keysize {
 		for b := 0; b < len(cipher); b += keysize {
